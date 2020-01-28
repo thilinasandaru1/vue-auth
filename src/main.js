b298ce02000eb640de48d42a73aea61f4df4ebb4
thilinasandaru1 <thilinasandaru1@gmail.com>
2020-01-28 16:18:59 +0530

    Setup Axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Axios from "axios";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.prototype.$http = Axios;
+const token = localStorage.getItem("token");
+
+if (token) {
+  Vue.prototype.$http.defaults.headers.common["Authorization"] = token;
+}
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
